4c9dbbddafcbf28a06fdb9c30773ae0eca94f7e5
StivenCarvajalCampus <StivenCarvajal3211@gmail.com>
2023-03-08 08:47:28 -0500

    feat(Add exercise): :sparkles: repeat()
    El metodo repeat()devuelve una cadena con un número de copias de una cadena.

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-/*El match()método compara una cadena con una expresión regular **
-
-El match()método devuelve una matriz con las coincidencias.
-
-El match()método devuelve nulo si no se encuentra ninguna coincidencia.*/
-let textmatch = "Vallenato mas que musica un sentimiento hecho cancion ";
-let result = textmatch.match(/to/g);
+//El repeat()método devuelve una cadena con un número de copias de una cadena.
+let text = "Vallenato ";
+let text2 = "Vallenato mas que musica un sentimiento hecho cancion";
+let result = text.repeat(4);
 console.log(result)
