54ddac68fee8e0e8ac125c3a6172eeba345279c3
StivenCarvajalCampus <StivenCarvajal3211@gmail.com>
2023-03-08 08:51:15 -0500

    feat(Add exercise): :sparkles: substr()
    método no cambia la cadena original.

--- main.js
+++ main.js
@@ -1,8 +1,6 @@
-//El slice()método extrae una parte de una cadena.
-/*Los parámetros de inicio y fin especifican la parte de la cadena que se va a extraer.
-La primera posición es 0, la segunda es 1, ...
-Un número negativo selecciona desde el final de la cadena.*/
+/*El substr()método no cambia la cadena original.
+Para extraer caracteres del final de la cadena, utilice una posición inicial negativa.*/
 let text = "Vallenato ";
 let text2 = "Vallenato mas que musica un sentimiento hecho cancion";
-let result = text.slice(0, 5);
+let result = text.substr(2);
 console.log(result)
