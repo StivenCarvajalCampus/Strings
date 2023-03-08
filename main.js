435f6c90264d0ed5716776dc5f10d8eb7392ab4b
StivenCarvajalCampus <StivenCarvajal3211@gmail.com>
2023-03-08 09:14:19 -0500

    feat(Add exercise): :sparkles: substring()
    Si el inicio es mayor que el final, los argumentos se intercambian: (4, 1) = (1, 4). Los valores iniciales o finales inferiores a 0 se tratan como 0.

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-/*El substr()método no cambia la cadena original.
-Para extraer caracteres del final de la cadena, utilice una posición inicial negativa.*/
+/* El substring()método no cambia la cadena original.
+Si el inicio es mayor que el final, los argumentos se intercambian: (4, 1) = (1, 4).
+Los valores iniciales o finales inferiores a 0 se tratan como 0.*/
 let text = "Vallenato ";
 let text2 = "Vallenato mas que musica un sentimiento hecho cancion";
-let result = text.substr(2);
+let result = text.substring(5);
 console.log(result)
