02f5c728410e57ca32f01f89611e4b6e5ec441dc
StivenCarvajalCampus <StivenCarvajal3211@gmail.com>
2023-03-08 06:32:09 -0500

    feat(Add exercise): :sparkles: .charAt()
    sirve para obtener el index de un caracter
    
    Agregué contenido en el index!

---
+++ main.js
@@ -0,0 +1,8 @@
+let text = "Vallenato ";
+let text2 = "Vallenato mas que musica un sentimiento hecho cancion";
+//Primer commit 
+/*El charAt()método devuelve el carácter en un índice especificado (posición) en una cadena.
+
+El índice del primer carácter es 0, el segundo 1, ...*/
+let letter = text.charAt(2);
+console.log(text.charAt(2));
