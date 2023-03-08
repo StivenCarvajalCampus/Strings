2b17f8955d5aa8dfd72788cb70bdc77595f5eb02
StivenCarvajalCampus <StivenCarvajal3211@gmail.com>
2023-03-08 06:36:54 -0500

    feat(Add exercise): :sparkles: .charCodeAt()
    método devuelve el Unicode del carácter en un índice especificado

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
+/*El charCodeAt()método devuelve el Unicode del carácter en un índice especificado (posición) en una cadena.
+
+El índice del primer carácter es 0, el segundo es 1, ....
+
+El índice del último carácter es la longitud de la cadena: 1 (consulte los ejemplos a continuación).*/
 let text = "Vallenato ";
 let text2 = "Vallenato mas que musica un sentimiento hecho cancion";
-//Primer commit 
-/*El charAt()método devuelve el carácter en un índice especificado (posición) en una cadena.
-
-El índice del primer carácter es 0, el segundo 1, ...*/
-let letter = text.charAt(2);
-console.log(text.charAt(2));
+let primero = text.charCodeAt(0)
+console.log(primero);
+//Ultimo
+let ultimo = text.charCodeAt(text.length -1);
+console.log(ultimo);
+//Por Defecto
+let pordefecto= text.charCodeAt()
+console.log(pordefecto)
+//Fuera de rango
+let noexiste = text.charCodeAt(22)
+console.log(noexiste) 
