b6eaa70b1dbbb9bf355b9d525f5f3f84f9bb1c14
StivenCarvajalCampus <StivenCarvajal3211@gmail.com>
2023-03-08 06:43:38 -0500

    feat(Add exercise): :sparkles: CharCode()
    charcode()

--- main.js
+++ main.js
@@ -1,5 +1,2 @@
-//charCodeAt()devuelve un número entre 0 y 65535.
-let text = "Vallenato ";
-let text2 = "Vallenato mas que musica un sentimiento hecho cancion";
-let point = text.codePointAt(1);
-console.log(point)
+let charc = String.fromCharCode(72, 69, 76, 76, 79);
+console.log(charc)
