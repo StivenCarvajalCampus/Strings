ecc10b80787913d4f0f261c40bfcc10d4af33da3
StivenCarvajalCampus <StivenCarvajal3211@gmail.com>
2023-03-08 06:39:54 -0500

    feat(Add exercise): :sparkles: .charCodeAt()
    devuelve un número entre 0 y 65535.

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
-/*El charCodeAt()método devuelve el Unicode del carácter en un índice especificado (posición) en una cadena.
-
-El índice del primer carácter es 0, el segundo es 1, ....
-
-El índice del último carácter es la longitud de la cadena: 1 (consulte los ejemplos a continuación).*/
+//charCodeAt()devuelve un número entre 0 y 65535.
 let text = "Vallenato ";
 let text2 = "Vallenato mas que musica un sentimiento hecho cancion";
-let primero = text.charCodeAt(0)
-console.log(primero);
-//Ultimo
-let ultimo = text.charCodeAt(text.length -1);
-console.log(ultimo);
-//Por Defecto
-let pordefecto= text.charCodeAt()
-console.log(pordefecto)
-//Fuera de rango
-let noexiste = text.charCodeAt(22)
-console.log(noexiste) 
+let point = text.codePointAt(1);
+console.log(point)
