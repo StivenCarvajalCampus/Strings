5ef9fe30570bce5d4c7483edf7b7d84ec55ef486
StivenCarvajalCampus <Stivencarvajal3211@gmail.com>
2023-03-08 12:56:39 -0500

    feat(Add exercise): :sparkles: split()
    Si (" ") se usa como separador, la cadena se divide entre palabras.

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-/* El substring()método no cambia la cadena original.
-Si el inicio es mayor que el final, los argumentos se intercambian: (4, 1) = (1, 4).
-Los valores iniciales o finales inferiores a 0 se tratan como 0.*/
 let text = "Vallenato ";
 let text2 = "Vallenato mas que musica un sentimiento hecho cancion";
-let result = text.substring(5);
-console.log(result)
+/*El split()método no cambia la cadena original.
+Si (" ") se usa como separador, la cadena se divide entre palabras.*/
+const myArray = text2.split(" ");
+console.log(myArray)
