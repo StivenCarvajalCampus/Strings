abfbf5fe0311cc34b4407e097596146f48b9d98e
StivenCarvajalCampus <StivenCarvajal3211@gmail.com>
2023-03-08 08:49:31 -0500

    feat(Add exercise): :sparkles: slice()
    método extrae una parte de una cadena.

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-//El repeat()método devuelve una cadena con un número de copias de una cadena.
+//El slice()método extrae una parte de una cadena.
+/*Los parámetros de inicio y fin especifican la parte de la cadena que se va a extraer.
+La primera posición es 0, la segunda es 1, ...
+Un número negativo selecciona desde el final de la cadena.*/
 let text = "Vallenato ";
 let text2 = "Vallenato mas que musica un sentimiento hecho cancion";
-let result = text.repeat(4);
+let result = text.slice(0, 5);
 console.log(result)
